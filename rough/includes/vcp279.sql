cc0fb4061313042c549e36a569b6f9170cb24164
David Herscher <david.herscher@gmail.com>
2013-11-24 22:21:54 -0500

    commit

--- rough/includes/vcp279.sql
+++ rough/includes/vcp279.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 09, 2013 at 09:48 PM
+-- Generation Time: Nov 25, 2013 at 03:20 AM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -59,8 +59,8 @@ INSERT INTO `equipment` (`id`, `serialNo`, `addedDate`, `name`, `description`, `
 CREATE TABLE IF NOT EXISTS `person` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `studentid` varchar(128) DEFAULT NULL,
-  `type` enum('Admin','Frontend','Student') NOT NULL,
-  `status` enum('active','disabled') NOT NULL,
+  `type` enum('Admin','Frontend','Student') NOT NULL DEFAULT 'Student',
+  `status` enum('active','disabled') NOT NULL DEFAULT 'active',
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `address1` varchar(255) DEFAULT NULL,
@@ -69,19 +69,38 @@ CREATE TABLE IF NOT EXISTS `person` (
   `email` varchar(128) DEFAULT NULL,
   `password` varchar(64) NOT NULL DEFAULT 'password',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `person`
 --
 
 INSERT INTO `person` (`id`, `studentid`, `type`, `status`, `firstname`, `lastname`, `address1`, `address2`, `zip`, `email`, `password`) VALUES
-(1, '00001', 'Admin', 'active', 'David', 'Herscher', '122 Gladstone Way', 'Newark DE', 17702, 'heer3906@chawk.cecil.edu', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(1, '00001', 'Admin', 'active', 'David', 'Herscher', '122 Gladstone Way', 'Newark DE', 17702, 'heer3906@chawk.cecil.edu', '4a23157209c98d318b78ee5678758df4'),
 (2, '00002', 'Student', 'active', 'Bruce', 'Lee', '441 Madstone Dr', 'Newark DE', 17702, 'brucelee@karate.com', '5f4dcc3b5aa765d61d8327deb882cf99'),
 (3, '00003', 'Student', 'active', 'Chuck', 'Norris', '356 Poopy Ct', 'Newark DE', 17702, 'walker@texaxranger.com', ''),
 (4, '45789', 'Frontend', 'active', 'Johny', 'Appleseed', '123 orchard way', 'Newark De', 19702, 'johny@appleseed.com', ''),
 (6, NULL, 'Student', 'active', 'Aaron', 'Weber', '122 Gladstone Way', 'Newark De', 19702, 'aaron@karate.com', ''),
-(7, NULL, 'Admin', 'active', 'Vcp', 'Admin', NULL, NULL, NULL, 'vcpadmin@vcp.com', '5f4dcc3b5aa765d61d8327deb882cf99');
+(7, '', 'Frontend', 'active', 'Vcp', 'Admin', '', '', 19702, 'vcpadmin@vcp.com', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(10, '123', 'Student', 'active', 'Test', 'Registration', '122 Gladstone Way', 'Bear De', 19702, 'test@register.com', '482c811da5d5b4bc6d497ffa98491e38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rentals`
+--
+
+CREATE TABLE IF NOT EXISTS `rentals` (
+  `rental_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `equipment_id` int(11) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_due` date NOT NULL,
+  `rental_status` enum('out','returned') CHARACTER SET utf8 NOT NULL,
+  `notes` varchar(512) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`rental_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
